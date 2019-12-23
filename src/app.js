1417cb43bf31fe1d895f0a71e50062013bef4917
Mike Blakley <m.blakley@icloud.com>
2019-12-23 14:18:55 -0500

    cors update

--- src/app.js
+++ src/app.js
@@ -17,9 +17,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors({
-  origin: CLIENT_ORIGIN
-}))
+app.use(cors())
 
 app.use('/api/breweries', breweriesRouter)
 app.use('/api/beers', beersRouter)
